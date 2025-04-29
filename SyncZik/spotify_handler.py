44d8a334e8d71b6a4c5b062c5c964dc6778f0d69
Dimitri Lerévérend <dimitri.lereverend@inria.fr>
2025-04-29 23:23:25 +0200

    Simple spotify playlist loader

--- SyncZik/spotify_handler.py
+++ SyncZik/spotify_handler.py
@@ -3,25 +3,30 @@ from config import SPOTIFY_CLIENT_ID, SPOTIFY_CLIENT_SECRET
 from spotipy.oauth2 import SpotifyClientCredentials
 
 
-def load_playlist(sp: spotipy.Spotify, spotiplaylist_id):
-    playlists = sp.user_playlists(spotiplaylist_id)
-    raise NotImplementedError
+def load_playlist(sp: spotipy.Spotify, playlist_id):
+    playlist = sp.playlist(playlist_id)
+    playlist_name = playlist["name"]
+    playlist_owner = playlist["owner"]["display_name"]
+    all_songs = []
+    tracks = playlist["tracks"]
+    while tracks:
+        for i, song in enumerate(tracks["items"]):
+            song_content = song["track"]
+            all_songs.append(song)
+            print(f"{len(all_songs)} {song_content['uri']} {song_content['name']}")
+        if tracks["next"]:
+            tracks = sp.next(tracks)
+        else:
+            tracks = None
+
+    return all_songs
 
 
 def main():
     auth_manager = SpotifyClientCredentials(SPOTIFY_CLIENT_ID, SPOTIFY_CLIENT_SECRET)
     sp = spotipy.Spotify(auth_manager=auth_manager)
 
-    playlists = sp.user_playlists("KEMIST")
-    while playlists:
-        for i, playlist in enumerate(playlists["items"]):
-            print(
-                f"{i + 1 + playlists['offset']:4d} {playlist['uri']} {playlist['name']}"
-            )
-        if playlists["next"]:
-            playlists = sp.next(playlists)
-        else:
-            playlists = None
+    load_playlist(sp, "2snz2HZz2ZWa6U3JK1hF19")
 
 
 if __name__ == "__main__":
